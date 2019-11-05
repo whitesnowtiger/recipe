d7c51210b67870dda8f5ea520f791530209425e6
Kimi <40905061+whitesnowtiger@users.noreply.github.com>
2019-11-04 22:01:33 -0800

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,4 +1,4 @@
 export const proxy = 'https://cors-anywhere.herokuapp.com/';
 //export const proxy = 'https://secret-ocean-49799.herokuapp.com/';
-export const key = '0b0d877bce468000318b82e6c8abf005';
+export const key = 'yourkey';
                     
